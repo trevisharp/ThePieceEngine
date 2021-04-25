b38779c8670d89c7937120f36abca455d1fcc240
trevisharp <38115923+trevisharp@users.noreply.github.com>
2021-04-25 07:03:11 -0300

    First Execution gitgnore addition

--- FrmMain.cs
+++ FrmMain.cs
@@ -10,11 +10,11 @@ using System.Windows.Forms;
 
 namespace ThePieceEngine
 {
-    public partial class Form1 : Form
+    public class FrmMain : Form
     {
-        public Form1()
+        public FrmMain()
         {
-            InitializeComponent();
+            
         }
 
     }
